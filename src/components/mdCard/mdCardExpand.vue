a090b0051fe24e443d037bd47106915510fed301
Ally Beaton <allanna.beaton@gmail.com>
2017-06-18 15:43:50 +1000

    Debugging statement

--- src/components/mdCard/mdCardExpand.vue
+++ src/components/mdCard/mdCardExpand.vue
@@ -8,6 +8,7 @@
   export default {
     methods: {
       setContentMargin() {
+        console.log('setContentMargin');
         this.content.style.marginTop = -this.content.offsetHeight + 'px';
       },
       toggle() {
